147eab93b2fb7a94c6f312b94e4abced8b6f0c42
Salma El Fathi <fathisalma374@gmail.com>
2026-03-13 15:50:19 +0000

    create delete cart item feature

--- src/pages/checkout/DeliveryDate.jsx
+++ src/pages/checkout/DeliveryDate.jsx
@@ -1,7 +1,14 @@
 import dayjs from "dayjs";
-export function DeliveryDate({deliveryOption,cartItem}) {
-    const deliveryoption=deliveryOption.find(option=>cartItem.deliveryOptionId===option.id);
-    const date=dayjs(deliveryoption.estimatedDeliveryTimeMs).format('dddd, MMMM D')
+export function DeliveryDate({ deliveryOption, cartItem }) {
+    if (!deliveryOption || !cartItem) return <div className="delivery-date">Loading...</div>;
+    const selectedOption = deliveryOption.find(
+        option => option.id === cartItem.deliveryOptionId
+    );
+
+    if (!selectedOption) {
+        return <div className="delivery-date">Delivery date: Not selected</div>;
+    }
+    const date = dayjs(selectedOption.estimatedDeliveryTimeMs).format('dddd, MMMM D');
     return (
         <div className="delivery-date">
             Delivery date: {date}
--- src/pages/checkout/OrderSummary.jsx
+++ src/pages/checkout/OrderSummary.jsx
@@ -1,25 +1,21 @@
 import { CartItemDetails } from './CartItemDetails';
 import { DeliveryOptions } from './DeliveryOptions';
 import { DeliveryDate } from './DeliveryDate';
-export function OrderSummary({cart,deliveryOption,loadCart}){
+export function OrderSummary({ cart, deliveryOption, loadCart }) {
     return (
-          <div className="order-summary">
-                        {cart.map(cartItem => {
-                            return (
-                                <div key={cartItem.id} className="cart-item-container">
-                                    
-
-                                    <div className="cart-item-details-grid">
-                                        <DeliveryDate deliveryOption={deliveryOption} cartItem={cartItem}/>
-                                        <CartItemDetails cartItem={cartItem} />
-                                        <DeliveryOptions deliveryOption={deliveryOption} cartItem={cartItem} loadCart={loadCart} />
-                                    </div>
-                                </div>
-                            )
-                        })
-                        }
-
-
+        <div className="order-summary">
+            {cart.map(cartItem => {
+                return (
+                    <div key={cartItem.id} className="cart-item-container">
+                        <div className="cart-item-details-grid">
+                            <DeliveryDate deliveryOption={deliveryOption} cartItem={cartItem} />
+                            <CartItemDetails cartItem={cartItem} loadCart={loadCart} />
+                            <DeliveryOptions deliveryOption={deliveryOption} cartItem={cartItem} loadCart={loadCart} />
+                        </div>
                     </div>
+                )
+            })
+            }
+        </div>
     );
 }
