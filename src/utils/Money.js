bcb1a44acf0d99b42bf2f9034550fdbff6a5e309
Salma El Fathi <fathisalma374@gmail.com>
2026-03-21 13:34:24 +0000

    add test negative and zero format money tests

--- src/utils/Money.js
+++ src/utils/Money.js
@@ -1,4 +1,4 @@
 
 export function formatMoney(amountCents){
-   return `$${(amountCents/100).toFixed(2)}`;
+   return `$${(Math.abs(amountCents)/100).toFixed(2)}`;
 }
--- src/utils/money.test.js
+++ src/utils/money.test.js
@@ -8,4 +8,10 @@ it('format 1999$ as 19.99$',()=>{
 it('displays 2 decimals',()=>{
     expect(formatMoney(1190)).toBe('$11.90')
 })
+it('works with 0',()=>{
+    expect(formatMoney(0)).toBe('$0.00')
+})
+it('works with negative numbers',()=>{
+expect(formatMoney(-999)).toBe('$9.99')
+})
 })
